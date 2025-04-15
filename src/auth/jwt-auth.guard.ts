7a3a35fc5a0feb855d6677abbc67a1e812b9a3a4
Gerasymov1 <a.k.gerasymov@gmail.com>
2025-04-15 20:56:59 +0300

    update jwt guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext } from '@nestjs/common/interfaces';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(
+    err: unknown,
+    user: any,
+    info: unknown,
+    context: ExecutionContext,
+  ) {
+    if (err || !user) {
+      throw new UnauthorizedException('No valid auth token');
+    }
+
+    return user;
+  }
+}
